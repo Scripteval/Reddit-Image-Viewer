3fe2c22c515906f2f23fb517867917ec392e35ed
wallace.m.brandon <wallace.m.brandon@gmail.com>
2016-09-22 23:27:48 -0400

    Basic functionality working.
    Can load direct image links from the front page of a subreddit.  No
    styling or testing yet.

---
+++ scraper.js
@@ -0,0 +1,23 @@
+function getImages(subreddit) {
+	$.getJSON("http://www.reddit.com/r/" + subreddit + "/.json?jsonp=?", 
+	function(data) { 
+	    $.each(data.data.children, function(i,item){
+	    	//If image link is valid, run this function and append to page
+	    	var appendImage = function(url, validUrl) {
+	    		if (validUrl) {
+	    			$("<img/>").attr("src", url).appendTo("#images");
+	    		}
+	    	}
+	    	var image = new Image();
+	    	var isValid = false;
+	    	image.onerror = image.onabort = function() {
+	    		appendImage(item.data.url, false);
+	    	};
+	    	image.onload = function() {
+	    		appendImage(item.data.url, true);
+	    	};
+	    	image.src = item.data.url;
+	    });
+	});
+}
+window.onload = getImages("anime_irl");
